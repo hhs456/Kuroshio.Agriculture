a095e47a6df030108b5371514756371f98269e3c
Hanson <75490747+hhs456@users.noreply.github.com>
2024-09-22 15:21:07 +0800

    feat(api)!: modify members in structure which declare for farm
    - add class `FarmData`
    - add interface `ISeedArgs`
    - modify members of struct `SeedData`

---
+++ Script/FarmData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kuroshio {
+    /// <summary>
+    /// 屯田資料結構 (暫定)
+    /// </summary>
+    [Serializable]
+    public class FarmData : ISeedArgs {
+        public int SeedId => seedId;
+        public bool IsPlanted => isPlanted;
+        public float Fertility => fertility;
+        public DateTime PlantTime => plantTime;
+        public TimeSpan ValidTime => validTime;
+        
+        public TimeSpan RipenTime { get; private set; }
+        public float ConsumptionRate { get; private set; }
+
+        [SerializeField] private int seedId;
+        [SerializeField] private bool isPlanted;
+        [SerializeField] private float fertility;
+        [SerializeField] private DateTime plantTime;
+        [SerializeField] private TimeSpan validTime;        
+
+        public void SetSeedArgs(SeedData data) {
+            ConsumptionRate = data.ConsumptionRate;
+            RipenTime = data.RipenTime;
+        }
+    }
+    /// <summary>
+    /// 種子資料結構 (暫定)
+    /// </summary>
+    [Serializable]
+
+    public struct SeedData : ISeedArgs {
+        public int SeedId => seedId;
+        public TimeSpan RipenTime => ripenTime;
+
+        public float ConsumptionRate => consumptionRate;
+
+        [SerializeField] private int seedId;
+        [SerializeField] private TimeSpan ripenTime;
+        [SerializeField] private float consumptionRate;
+
+        public SeedData(int seedId, TimeSpan ripenTime, float consumptionRate) {
+            this.seedId = seedId;
+            this.ripenTime = ripenTime;
+            this.consumptionRate = consumptionRate;
+        }
+    }
+
+    public interface ISeedArgs {
+        float ConsumptionRate { get; }
+        TimeSpan RipenTime { get; }
+    }
+}
