2044fc500dc550abe856cca911d87e16e6517009
Hanson <75490747+hhs456@users.noreply.github.com>
2024-10-02 19:15:06 +0800

    feat!(prop): exchange type of `seedId` from `int` to`string`

--- Script/FarmData.cs
+++ Script/FarmData.cs
@@ -9,20 +9,24 @@ namespace Kuroshio.Agriculture {
     /// </summary>
     [Serializable]
     public class FarmData : ISeedArgs {
-        public int SeedId => seedId;
+        public string SeedId => seedId;
         public bool IsPlanted => isPlanted;
-        public float Fertility => fertility;
+        public float Fertility { get => fertility; set => fertility = value; }
         public DateTime PlantTime => plantTime;
         public TimeSpan ValidTime => validTime;
         
         public TimeSpan RipenTime { get; private set; }
         public float ConsumptionRate { get; private set; }
 
-        [SerializeField] private int seedId;
+        [SerializeField] private string seedId;
         [SerializeField] private bool isPlanted;
         [SerializeField] private float fertility;
         [SerializeField] private DateTime plantTime;
-        [SerializeField] private TimeSpan validTime;        
+        [SerializeField] private TimeSpan validTime;
+
+        public FarmData(string seedId) {
+            this.seedId = seedId;
+        }
 
         public void SetSeedArgs(SeedData data) {
             ConsumptionRate = data.ConsumptionRate;
@@ -35,16 +39,16 @@ namespace Kuroshio.Agriculture {
     [Serializable]
 
     public struct SeedData : ISeedArgs {
-        public int SeedId => seedId;
+        public string SeedId => seedId;
         public TimeSpan RipenTime => ripenTime;
 
         public float ConsumptionRate => consumptionRate;
 
-        [SerializeField] private int seedId;
+        [SerializeField] private string seedId;
         [SerializeField] private TimeSpan ripenTime;
         [SerializeField] private float consumptionRate;
 
-        public SeedData(int seedId, TimeSpan ripenTime, float consumptionRate) {
+        public SeedData(string seedId, TimeSpan ripenTime, float consumptionRate) {
             this.seedId = seedId;
             this.ripenTime = ripenTime;
             this.consumptionRate = consumptionRate;
